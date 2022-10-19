a9430e4fa1b26197fa8853f90f06f08d6d559364
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-10-19 22:21:35 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const mainGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+mainGallery.innerHTML = galleryItemsMarkup;
+
+mainGallery.addEventListener('click', onClick);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250}); //!создаст слайдер с задержкой и подписью сверху
+
+function createGalleryItems(galleryItems) { //!создание разметки из объекта
+    const markup = galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href=${original}>
+            <img class="gallery__image" src=${preview} alt=${description} />
+        </a>
+        `;
+    }).join('');
+    return markup;
+}
+
+function onClick(evt) { //? отмена действий от браузера по умолчанию
+    evt.preventDefault();
+}
+
+
