ab29500625f71234aab2600862212dc65e4e46f1
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-10-20 01:11:25 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,23 @@
-import Player from '@vimeo/player';
+import Player from '@vimeo/player'; //! из документации
+// import throttle from 'lodash.throttle';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe'); //! из документации
+const player = new Player(iframe); //! из документации
+const STORAGE_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+lastTime(); //! если localStorage не пустой начнёт воспроизведение с последнего сохранённого времени
+
+function playTime(data) { //? Функция запишет в localStorage секунды воспроизведения
+    localStorage.setItem(STORAGE_TIME, data.seconds)
+}
+
+function lastTime() {//? Функция запишет в localStorage секунды воспроизведения и при перезагрузке вызовет setCurrentTime с сохр.временем
+    const setTime = localStorage.getItem(STORAGE_TIME);
+
+    if (setTime) {
+        player.setCurrentTime(setTime);
+    }
+}
