04cbd1da56efcf6c213ca7f0b3b096fd2101b5c0
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-10-22 00:39:31 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
-var throttle = require('lodash.throttle'); //! из документации
+var throttle = require('lodash.throttle'); //! из документации throttle
+
+
+const formRef = document.querySelector('.feedback-form');
+const KEY = 'feedback-form-state';
+const data = {email:"", message: ""};
+
+formRef.addEventListener('submit', submitFunc );
+formRef.addEventListener('input', throttle(inputFunc, 500));
+
+resumeInput();
+
+
+function submitFunc(evt) { //! После submit обнуляет currentTarget и поле ввода, и убрали действие по умолчанию,выведет dataParced
+    evt.preventDefault();
+    const parceSaved = localStorage.getItem(KEY);
+    const dataParced = JSON.parse(parceSaved);
+    if (dataParced) {
+        console.log(dataParced);
+    }
+    else {
+        console.log(data);
+    }
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(KEY);
+    data.email = '';
+    data.message = '';
+}
+
+function inputFunc(evt) { //! Приведение значений к записи в объект
+    data[evt.target.name] = evt.target.value;
+    const dataJSON = JSON.stringify(data);
+    localStorage.setItem(KEY, dataJSON);
+}
+
+function resumeInput(evt) { //! Востановление значений из localStorage
+    const savedInput = localStorage.getItem(KEY);
+    const ParsesavedInput = JSON.parse(savedInput);
+
+    if (ParsesavedInput) {
+        const inputRef = formRef.querySelector('input');
+        const textareaRef = formRef.querySelector('textarea');
+        inputRef.value = ParsesavedInput.email ? ParsesavedInput.email : '';
+        textareaRef.value = ParsesavedInput.message ? ParsesavedInput.message : '';
+        data.email = ParsesavedInput.email;
+        data.message = ParsesavedInput.message;
+    }
+}
